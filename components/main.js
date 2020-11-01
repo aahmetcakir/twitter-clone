a93a76b22d3b52d4136e06e72ebd6a6d16e30533
Ahmet ÇAKIR <ca.ahmetcakir@gmail.com>
2020-11-01 22:24:12 +0300

    yazılar düzeldi

--- components/main.js
+++ components/main.js
@@ -21,32 +21,31 @@ function Main() {
           'https://pbs.twimg.com/profile_images/1322853331073671169/bcNaQF8Y_400x400.jpg'
         }
       >
-        Bu ay boyunca 14 yıllık SEO, 8 yıllık ajans yönetimi, 2 yıllık ürün
-        yönetiminde yaptığım hatalardan öğrendiklerimi sorularınızı
-        yanıtlandırmak için paylaşacağım. Tüm gelirleri @superpeer üzerinden
-        @AKUT_Dernegi 'ne İzmir için bağışlanacak. Destek için:
-        https://superpeer.com/yigit{' '}
+        bu kadar hızlı değişen ve gelişen teknoloji dünyasında ürün satın almak
+        çok manasız geliyor her defasında. telefondaki uygulama mağazası gibi
+        her şeyde abonelik modeli olsa fena olmaz. min 3 aylık, 6 aylık, veya
+        yıllık. elimdekini satıp yenisini alayım derdi ortadan kalkıyor.{' '}
       </TweetPhoto>
       <TweetPhoto
-        dk={'8d'}
+        dk={'24 May 2018 '}
         pp={
           'https://pbs.twimg.com/profile_images/1322853331073671169/bcNaQF8Y_400x400.jpg'
         }
       >
-        Bu ay boyunca 14 yıllık SEO, 8 yıllık ajans yönetimi, 2 yıllık ürün
-        yönetiminde yaptığım hatalardan öğrendiklerimi sorularınızı
-        yanıtlandırmak için paylaşacağım. Tüm gelirleri @superpeer üzerinden
-        @AKUT_Dernegi 'ne İzmir için bağışlanacak. Destek için:
-        https://superpeer.com/yigit{' '}
+        facebook, airbnb, stripe, snapchat, .. bu şirketlerden birinde çalışmak
+        ister misin? şu linki baştan sona bitir, ingilizceni akıcı hale getir,
+        teknik mülakat konusunda kendini geliştir. senelik min. 100 bin dolar
+        maaş garanti Oldu{' '}
       </TweetPhoto>
       <TweetNormal
         pp={
           'https://pbs.twimg.com/profile_images/1322853331073671169/bcNaQF8Y_400x400.jpg'
         }
-        dk={'15d'}
+        dk={'46d'}
       >
-        MIT’de verilen ‘bilgisayar muhendisiligindeki eksik donem’ adli ders
-        TR’ye tamamen cevrilmis
+        “zenginler varlığını yatırım için kullanırken fakirler zenginmiş gibi
+        parasını harcıyor.” — sizce bireylerin bulunduğu finansal konum için
+        verdiği kararlar, tercihleri ne kadar önemli?
       </TweetNormal>
       <TweetNormal
         pp={
@@ -58,16 +57,15 @@ function Main() {
         TR’ye tamamen cevrilmis
       </TweetNormal>
       <TweetPhoto
-        dk={'8d'}
+        dk={'26 Eki'}
         pp={
           'https://pbs.twimg.com/profile_images/1322853331073671169/bcNaQF8Y_400x400.jpg'
         }
       >
-        Bu ay boyunca 14 yıllık SEO, 8 yıllık ajans yönetimi, 2 yıllık ürün
-        yönetiminde yaptığım hatalardan öğrendiklerimi sorularınızı
-        yanıtlandırmak için paylaşacağım. Tüm gelirleri @superpeer üzerinden
-        @AKUT_Dernegi 'ne İzmir için bağışlanacak. Destek için:
-        https://superpeer.com/yigit{' '}
+        kullanmadığım hiç bi şeyi anlatmadım. gerçekten bildiğim ve canlıya
+        çıkmış projelerde kullandığım araçları anlattım. yeni bi şey öğrenme
+        kısmı ise proje yaparken öğrenirim yani bunu öğreneyim lazım olur diye
+        bi şey öğrenmem, fikrim olması için neyi çözdüğüne bakarım.{' '}
       </TweetPhoto>
     </div>
   )
